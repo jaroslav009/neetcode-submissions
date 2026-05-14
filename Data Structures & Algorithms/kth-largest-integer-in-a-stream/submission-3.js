f64a00c652f8087ff55c3ee7a9146f313b688f31
Yaroslav <33982583+jaroslav009@users.noreply.github.com>
2026-05-14 17:25:53 +0300

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.js
@@ -0,0 +1,29 @@
+class KthLargest {
+    data = []
+    largestNum = 0
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.data = [...nums]
+        this.largestNum = k
+    }
+    // [1, 2, 3, 3, 3] 3
+    // [1, 2, 3, 3, 3, 5] 3
+    //  [1, 2, 3, 3, 3, 5, 6] 3
+    //  [1, 2, 3, 3, 3, 5, 6, 7] 5
+    //  [1, 2, 3, 3, 3, 5, 6, 7, 8] 6
+    // 
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.data.push(val)
+        if (this.data.length) {
+            return this.data.toSorted((a,b) => a - b)[this.data.length - this.largestNum]
+        }
+        // return null
+    }
+}
