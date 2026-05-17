d0c7ddb8add2bcf2ee4a7a1d38f01e5a05d04ad3
Yaroslav <33982583+jaroslav009@users.noreply.github.com>
2026-05-17 10:57:20 +0300

    Add: crawler-log-folder - submission-1

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} logs
+     * @return {number}
+     */
+    minOperations(logs) {
+        let currentPosition = 0
+        for (let i = 0; i < logs.length; i++) {
+            switch (logs[i]) {
+                case "../":
+                    if (currentPosition > 0) {
+                        currentPosition--
+                    }
+                    break;
+                case "./":
+                    break;
+                default:
+                    currentPosition++
+            }
+        }
+        return currentPosition
+    }
+}
