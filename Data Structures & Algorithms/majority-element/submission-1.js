b893d6707a9dbbcce280b2f15c5ab03d052260a3
Yaroslav <33982583+jaroslav009@users.noreply.github.com>
2026-05-18 10:57:25 +0300

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let hash = {}
+        let major = {count: null, val: null}
+        for (let i = 0; i < nums.length; i++) {
+            hash[nums[i]] = (hash[nums[i]] ?? 0) + 1
+            if (hash[nums[i]] > major.count) {
+                major.count = hash[nums[i]]
+                major.val = nums[i]
+            }
+        }
+        return major.val
+    }
+}
