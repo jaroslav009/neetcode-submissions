90893425c6b3861e57b47b9da72a86829bfaddb7
Yaroslav <33982583+jaroslav009@users.noreply.github.com>
2026-05-24 08:06:43 +0300

    Add: roman-to-integer - submission-1

---
+++ Data Structures & Algorithms/roman-to-integer/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    transformToNumber(symbol) {
+        switch (symbol) {
+            case "I":
+                return 1
+            case "V":
+                return 5
+            case "X":
+                return 10
+            case "L":
+                return 50
+            case "C":
+                return 100
+            case "D":
+                return 500
+            case "M":
+                return 1000
+        }
+    }
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    romanToInt(s) {
+        let sum = 0;
+        for (let i = 0; i < s.length; i++) {
+            const currentSymbol = this.transformToNumber(s[i])
+            if (this.transformToNumber(s[i + 1]) > currentSymbol) {
+                sum -= currentSymbol
+            } else {
+                sum += currentSymbol
+            }
+        }
+        return sum
+    }
+}
